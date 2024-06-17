88cf366e936753f081519f9ba9deb1d197682fd3
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2024-06-17 23:11:04 +0530

    Update LocationTime component

--- src/components/LocationTime/LocationTime.js
+++ src/components/LocationTime/LocationTime.js
@@ -1,12 +1,27 @@
+import { DateTime } from "luxon";
 import "./LocationTime.css";
 
 export function LocationTime({
-    time,
     location,
-    day,
-    date,
-    alignment
+    timezone = "local",
+    alignment = "left",
 }) {
+    let time = null;
+    let day = null;
+    let date = null;
+    
+    // By default, as local timezone is selected, we create date in local time
+    let dt = DateTime.local();
+
+    // For timezones other than local
+    if(timezone !== "local") {
+        dt = dt.setZone(timezone);
+    }
+
+    time = dt.toLocaleString(DateTime.TIME_24_SIMPLE);
+    day = dt.toLocaleString({ weekday: 'long' });
+    date = dt.toLocaleString({ day: 'numeric', month: 'short' })
+
     return(
         <div className={ alignment && alignment === "right" ? "location-time flex-align-end" : "location-time flex-align-start" }>
             <h2>{ time }</h2>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,10 +5,7 @@ export function Navbar() {
     return(
         <nav>
             <LocationTime
-                time={ "18:00" }
-                location={ "Minsk" }
-                day={ "Friday" }
-                date={ "26 Nov" }
+                location={ "Pune" }
             />
             <div>
                 <button>+</button>
