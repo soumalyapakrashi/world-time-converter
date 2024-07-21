e16f7f959b88f0c1bf99bac70de7d5dae73dc502
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2024-07-21 22:56:41 +0530

    Update timezonecard to fetch timezone and time offset

--- src/components/TimeZoneCard/TimeZoneCard.js
+++ src/components/TimeZoneCard/TimeZoneCard.js
@@ -1,11 +1,5 @@
-/*
-    Receive: City code
-    Responsibilities:
-    Fetch country
-    Fetch current time in that city
-    Fetch a picture of that city
-*/
-
+import { useEffect, useState } from "react";
+import { DateTime } from "luxon";
 import { LocationTime } from "../LocationTime/LocationTime";
 import "./TimeZoneCard.css";
 
@@ -13,8 +7,26 @@ export function TimeZoneCard({
     city,
     country,
     pictureUrl,
-    timeOffset,
+    latitude,
+    longitude
 }) {
+    const [ timezone, setTimezone ] = useState("local");
+    const [ datetimeObj, setDatetimeObj ] = useState(DateTime.local());
+
+    useEffect(() => {
+        fetch(`https://api.weatherapi.com/v1/timezone.json?key=${process.env.REACT_APP_WEATHERAPI_KEY}&q=${latitude},${longitude}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            setTimezone(data.location.tz_id);
+            setDatetimeObj(prevState => prevState.setZone(timezone));
+        });
+    }, [ latitude, longitude, timezone ]);
+
+    const offsetInMinutes = datetimeObj.offset;
+    const offsetInHours = offsetInMinutes / 60;
+
     return(
         <div className="card">
             <div className="card-container">
@@ -23,12 +35,12 @@ export function TimeZoneCard({
                 </div>
                 <div className="card-body">
                     <div>
-                        <h3>{ city } ({ timeOffset })</h3>
+                        <h3>{ city } ({ `UTC${offsetInHours > 0 ? "+" : ""}${offsetInHours}` })</h3>
                         <h4>{ country }</h4>
                     </div>
                     <LocationTime
                         alignment={ "right" }
-                        timezone={ "Australia/Sydney" }
+                        timezone={ timezone }
                     />
                 </div>
             </div>
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -17,14 +17,16 @@ export function Home() {
                     <TimeZoneCard
                         city={ "Sydney" }
                         country={ "Australia" }
-                        timeOffset={ "GMT+10" }
                         pictureUrl={ "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2023/02/10095720/hifi-tyler-duston-unsplash-tlhero.jpeg?tr=w-600" }
+                        latitude={ -33.86 }
+                        longitude={ 151.21 }
                     />
                     <TimeZoneCard
                         city={ "New York" }
                         country={ "USA" }
-                        timeOffset={ "GMT-5" }
                         pictureUrl={ "https://content.paulreiffer.com/wp-content/uploads/2014/12/Stone-And-Steel-New-York-City-Brooklyn-Bridge-Freedom-Tower-One-World-Trade-Center-Financial-District-FiDi-Manhattan-Long-Exposure-Night-Sunset-Lights-Cityscape-Paul-Reiffer-Photographer@2x.jpg" }
+                        latitude={ 40.71 }
+                        longitude={ -74.00 }
                     />
                 </div>
             </div>
