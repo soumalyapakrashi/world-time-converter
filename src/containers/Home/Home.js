347cf95e8d46763bc4f62a2d4b98c8cf93fc3232
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2024-07-22 00:29:47 +0530

    Handle demo data in state and remove hard-typed data

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { TimeZoneCard } from "../../components/TimeZoneCard/TimeZoneCard";
 import { WorldMap } from "../../components/WorldMap/WorldMap";
 import "./Home.css";
 
 export function Home() {
+    const [ locationData, setLocationData ] = useState([
+        {
+            city: "Sydney",
+            country: "Australia",
+            pictureUrl: "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2023/02/10095720/hifi-tyler-duston-unsplash-tlhero.jpeg?tr=w-600",
+            latitude: -33.86,
+            longitude: 151.21
+        },
+        {
+            city: "New York",
+            country: "USA",
+            pictureUrl: "https://content.paulreiffer.com/wp-content/uploads/2014/12/Stone-And-Steel-New-York-City-Brooklyn-Bridge-Freedom-Tower-One-World-Trade-Center-Financial-District-FiDi-Manhattan-Long-Exposure-Night-Sunset-Lights-Cityscape-Paul-Reiffer-Photographer@2x.jpg",
+            latitude: 40.71,
+            longitude: -74.00
+        }
+    ])
+
     return(
         <div className="main-container">
             <div className="margin-container">
@@ -14,20 +32,17 @@ export function Home() {
                 />
 
                 <div className="timezone-card-grid">
-                    <TimeZoneCard
-                        city={ "Sydney" }
-                        country={ "Australia" }
-                        pictureUrl={ "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2023/02/10095720/hifi-tyler-duston-unsplash-tlhero.jpeg?tr=w-600" }
-                        latitude={ -33.86 }
-                        longitude={ 151.21 }
-                    />
-                    <TimeZoneCard
-                        city={ "New York" }
-                        country={ "USA" }
-                        pictureUrl={ "https://content.paulreiffer.com/wp-content/uploads/2014/12/Stone-And-Steel-New-York-City-Brooklyn-Bridge-Freedom-Tower-One-World-Trade-Center-Financial-District-FiDi-Manhattan-Long-Exposure-Night-Sunset-Lights-Cityscape-Paul-Reiffer-Photographer@2x.jpg" }
-                        latitude={ 40.71 }
-                        longitude={ -74.00 }
-                    />
+                    { locationData.map(data => {
+                        return(
+                            <TimeZoneCard
+                                city={ data.city }
+                                country={ data.country }
+                                pictureUrl={ data.pictureUrl }
+                                latitude={ data.latitude }
+                                longitude={ data.longitude }
+                            />
+                        )
+                    })}
                 </div>
             </div>
         </div>
